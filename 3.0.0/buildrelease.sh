010346d120fd3d24c5d218d9c0d6fa6b790137cf
Eric W. Brown <feneric@gmail.com>
2014-11-27 10:39:39 -0500

    Added accidentally missing build lines to the main build script.

--- 3.0.0/buildrelease.sh
+++ 3.0.0/buildrelease.sh
@@ -5,5 +5,7 @@ TODAY = `date “+%Y%m%d”`
 ./emotion-generic-playersbuild.sh $TODAY 1.11.0
 ./elementarybuild.sh $TODAY 1.11.2
 ./python-eflbuild.sh $TODAY 1.11.0
+./epulsebuild.sh $TODAY
+./e19build.sh $TODAY 1.11.2
 ./terminologybuild.sh $TODAY 0.6.1
 ./econnmanbuild.sh $TODAY 1.1
